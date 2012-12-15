afcc3a21da9f91b036b445aea9d5d5ee68826fee
Olga.Konoreva <olga.konoreva@aldigit.com>
2012-12-15 17:00:29 +0700

    added rondom movement

--- js/board.js
+++ js/board.js
@@ -7,7 +7,7 @@ var app = app || {};
 	}
 
 	Board.prototype.addObject = function(url) {
-		var obj = new app.FlyObject(this.context, url, { x:0, y:0, width: 120, height:120 });
+		var obj = new app.FlyObject(this.context, url, { x:30, y:50, width: 120, height:120 });
 	};
 
 	app.Board = Board;
--- js/object.js
+++ js/object.js
@@ -4,7 +4,7 @@ var app = app || {};
 
 
 	function FlyObject(context, url, rect, velocity, moveCallback) {
-		this.velocity = velocity || 10;
+		this.velocity = velocity || 5;
 		this.context = context;
 		this.x = rect.x;
 		this.y = rect.y;
@@ -26,24 +26,43 @@ var app = app || {};
 	};
 
 	FlyObject.prototype.move = function() {
-		this.moveObject(1,1);
-	}	
-
-	FlyObject.prototype.removeObject = function() {
-		this.context.clearRect( this.x, this.y, this.width, this.height );
+		var min = -30, max = 150;
+		var vectorX = Math.floor(Math.random() * (max - min + 1)) + min;
+		var vectorY = Math.floor(Math.random() * (max - min + 1)) + min;
+		this.moveTo( this.x + vectorX, this.y + vectorY);
 	}
 
-	FlyObject.prototype.moveObject = function(dx, dy) {
+	FlyObject.prototype.removeObject = function() {
+		this.context.clearRect( this.x - 1, this.y - 1, this.width + 2, this.height + 2 );
+	}
+
+	FlyObject.prototype.moveTo = function( x, y ) {
+		var dx = x - this.x,
+			dy = y - this.y;
+		var length = Math.abs( dx ) >= Math.abs( dy ) ? Math.abs( dx ) : Math.abs( dy) ;
+
+		this.moveObject( dx/length, dy/length, x, y );
+	};
+
+	FlyObject.prototype.moveObject = function(dx, dy, pointX, pointY) {
 		var self = this;
 		this.redrawImage( dx, dy );
-		console.log( this.y );
+
+		if ( this.richPoint( pointX, pointY ) ) {
+			self.move();
+			return;	
+		} 
 
 		this.outOfBorder() ? this.moveCallback() :
-				setTimeout( function () { self.moveObject(1,1) }, self.velocity );
+				setTimeout( function () { self.moveObject( dx,dy, pointX, pointY ) }, self.velocity );
 	};
 
 	FlyObject.prototype.outOfBorder = function() {
 		return this.x >= BOARD_WIDTH || this.y >= BOARD_HEIGHT;
+	};
+
+	FlyObject.prototype.richPoint = function( pointX, pointY ) {
+		return Math.abs(this.x - pointX) <= 1 && Math.abs(this.y - pointY) <= 1;
 	};
 
 	FlyObject.prototype.redrawImage = function( dx, dy ) {
