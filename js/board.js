b2b873115377d0d71f41f6d28e3632dc74541aec
Olga.Konoreva <olga.konoreva@aldigit.com>
2012-12-15 13:43:30 +0700

    added movement

--- js/board.js
+++ js/board.js
@@ -7,7 +7,7 @@ var app = app || {};
 	}
 
 	Board.prototype.addObject = function(url) {
-		var obj = new app.FlyObject(this.context, url);
+		var obj = new app.FlyObject(this.context, url, { x:0, y:0, width: 120, height:120 });
 	};
 
 	app.Board = Board;
--- js/object.js
+++ js/object.js
@@ -3,16 +3,43 @@ var app = app || {};
 (function() {
 
 
-	function FlyObject(context, url) {
+	function FlyObject(context, url, rect, velocity) {
+		this.velocity = velocity || 10;
 		this.context = context;
+		this.x = rect.x;
+		this.y = rect.y;
+		this.width = rect.width;
+		this.height = rect.height;
 
-		var image = new Image();
-		image.src = url;
+		var self = this;
 
-		image.onload = function() {  // Событие onLoad, ждём момента пока загрузится изображение
-      		context.drawImage(image, 0, 0);  // Рисуем изображение от точки с координатами 0, 0
+		self.image = new Image();
+		self.image.src = url;
+
+		self.image.onload = function() {  // Событие onLoad, ждём момента пока загрузится изображение
+      		context.drawImage( self.image, self.x, self.y, self.width, self.height );  // Рисуем изображение от точки с координатами 0, 0
   		}
+
+  		self.move();
+
+	};
+
+	FlyObject.prototype.move = function() {
+		this.moveObject(1,1);
+	}	
+
+	FlyObject.prototype.removeObject = function() {
+		this.context.clearRect( this.x, this.y, this.width, this.height );
 	}
+
+	FlyObject.prototype.moveObject = function(dx, dy) {
+		var self = this;
+		this.removeObject();
+		this.context.drawImage( this.image, this.x + dx, this.y + dy, this.width, this.height );
+		this.x += dx;
+		this.y += dy;
+		setTimeout( function () { self.moveObject(1,1) }, self.velocity );
+	};
 
 	app.FlyObject = FlyObject;
 
